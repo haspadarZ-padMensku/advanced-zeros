4b8a5c90f19c4c1b9709f243f9b7762a6d804724
haspadarZ-padMensku <gonchds@gmail.com>
2018-09-16 17:25:44 +0300

    implement getZerosCount func w 2 failed tests...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let n = 2;
+  let multipliers = [];
+
+  while (n <= base) {
+    if (base % n == 0) {
+      multipliers.push(n);
+      base /= n;
+    } else {
+      n++;
+    }
+  }
+
+  n = multipliers[multipliers.length - 1];
+
+  let counter = 0;
+
+  while (number >= n) {
+    number = Math.floor(number / n);
+    counter += number;
+  }
+
+  let repeat = 0;
+
+  while (multipliers[multipliers.length - 1] ==
+    multipliers[multipliers.length - 2 - repeat] ) {
+    repeat ++;
+  }
+
+  return Math.floor(counter / (repeat + 1));
 }
